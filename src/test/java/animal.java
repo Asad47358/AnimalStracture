b54a4fd41ab8292e3ac520a2f347c2a00072eefc
Asad47358 <asadullahahmadzad@gmail.com>
2024-11-17 21:28:58 -0800

    he create hasib class

--- src/test/java/animal.java
+++ src/test/java/animal.java
@@ -2,6 +2,10 @@ public class animal {
     public static void main(String[] args) {
         base b = new base();
         b.name="dog";
+
+        hasib hasib = new hasib();
+        hasib.name="ahmadzad";
+        
     }
    
 }
